659ee683519e7406287c6b7664ae27135e16c252
JHwang95 <1995.j.hwang@gmail.com>
2023-02-15 09:26:09 -0500

    added all necessary files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    /**
+     * The starting point of the application
+     */
+    public static void main(String[] args)
+    {
+        // File is stored in a resources folder in the project
+        final String filename = "src/TheLOrdOfTheRings.txt";
+        int count = 0;
+        try {
+            Scanner fin = new Scanner(new File(filename));
+            fin.useDelimiter("\\s|\"|\\(|\\)|\\.|\\,|\\?|\\!|\\_|\\-|\\:|\\;|\\n");  // Filter - DO NOT CHANGE
+            while (fin.hasNext()) {
+                String fileWord = fin.next().toLowerCase();
+                if (fileWord.length() > 0)
+                {
+                    // Just print to the screen for now - REMOVE once you have completed code
+                    System.out.printf("%s\n", fileWord);
+                    count++;
+                }
+            }
+            fin.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
+
+        System.out.println("There are " + count + " words in thew file ");
+        // ADD other code after here
+
     }
 }
